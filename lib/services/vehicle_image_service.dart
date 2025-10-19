8c35a75cec9676ae999126635f01620516b5ec6f
Johan <johan@example.com>
2025-10-19 15:06:15 -0500

    Expandir significativamente marcas y modelos de vehiculos disponibles

--- lib/services/vehicle_image_service.dart
+++ lib/services/vehicle_image_service.dart
@@ -99,105 +99,206 @@ class VehicleImageService {
 
   // Sistema de marcas y modelos por tipo de vehículo
   static const Map<String, List<String>> vehicleBrands = {
-    'Carro': ['Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Mazda', 'Mitsubishi', 'Personalizado'],
-    'Sedán': ['Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Mazda', 'BMW', 'Mercedes-Benz', 'Audi', 'Personalizado'],
-    'SUV': ['Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Mazda', 'BMW', 'Mercedes-Benz', 'Audi', 'Personalizado'],
-    'Hatchback': ['Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Mazda', 'Peugeot', 'Renault', 'Personalizado'],
-    'Motocicleta': ['Honda', 'Yamaha', 'Suzuki', 'Kawasaki', 'KTM', 'Ducati', 'Harley-Davidson', 'BMW', 'Aprilia', 'Personalizado'],
-    'Camión': ['Ford', 'Chevrolet', 'GMC', 'Ram', 'Toyota', 'Nissan', 'Isuzu', 'Mitsubishi', 'Personalizado'],
-    'Van': ['Ford', 'Chevrolet', 'Nissan', 'Toyota', 'Mercedes-Benz', 'Volkswagen', 'Renault', 'Peugeot', 'Personalizado'],
+    'Carro': [
+      'Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Mazda', 'Mitsubishi',
+      'BMW', 'Mercedes-Benz', 'Audi', 'Lexus', 'Infiniti', 'Acura', 'Genesis', 'Subaru', 'Volvo', 'Jaguar',
+      'Land Rover', 'Porsche', 'Bentley', 'Rolls-Royce', 'Ferrari', 'Lamborghini', 'Maserati', 'Alfa Romeo',
+      'Fiat', 'Peugeot', 'Renault', 'Citroën', 'Seat', 'Skoda', 'Dacia', 'Opel', 'Saab', 'Lada',
+      'Tata', 'Mahindra', 'BYD', 'Geely', 'Great Wall', 'Chery', 'JAC', 'Dongfeng', 'FAW', 'Personalizado'
+    ],
+    'Sedán': [
+      'Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Mazda', 'BMW',
+      'Mercedes-Benz', 'Audi', 'Lexus', 'Infiniti', 'Acura', 'Genesis', 'Volvo', 'Jaguar', 'Porsche',
+      'Bentley', 'Rolls-Royce', 'Ferrari', 'Lamborghini', 'Maserati', 'Alfa Romeo', 'Fiat', 'Peugeot',
+      'Renault', 'Citroën', 'Seat', 'Skoda', 'Opel', 'Saab', 'Subaru', 'Mitsubishi', 'Personalizado'
+    ],
+    'SUV': [
+      'Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Mazda', 'BMW',
+      'Mercedes-Benz', 'Audi', 'Lexus', 'Infiniti', 'Acura', 'Genesis', 'Volvo', 'Jaguar', 'Land Rover',
+      'Porsche', 'Bentley', 'Rolls-Royce', 'Lamborghini', 'Maserati', 'Alfa Romeo', 'Fiat', 'Peugeot',
+      'Renault', 'Citroën', 'Seat', 'Skoda', 'Opel', 'Subaru', 'Mitsubishi', 'Jeep', 'Ram', 'GMC',
+      'Cadillac', 'Lincoln', 'Buick', 'Personalizado'
+    ],
+    'Hatchback': [
+      'Toyota', 'Honda', 'Nissan', 'Hyundai', 'Kia', 'Chevrolet', 'Ford', 'Volkswagen', 'Cupra', 'Mazda',
+      'BMW', 'Mercedes-Benz', 'Audi', 'Mini', 'Fiat', 'Peugeot', 'Renault', 'Citroën', 'Seat', 'Skoda',
+      'Opel', 'Saab', 'Subaru', 'Mitsubishi', 'Suzuki', 'Daihatsu', 'Smart', 'Abarth', 'Alfa Romeo', 'Personalizado'
+    ],
+    'Motocicleta': [
+      'Honda', 'Yamaha', 'Suzuki', 'Kawasaki', 'KTM', 'Ducati', 'Harley-Davidson', 'BMW', 'Aprilia',
+      'Triumph', 'Royal Enfield', 'Bajaj', 'TVS', 'Hero', 'Benelli', 'MV Agusta', 'Moto Guzzi', 'Piaggio',
+      'Vespa', 'Husqvarna', 'GasGas', 'Beta', 'Sherco', 'Husaberg', 'Cagiva', 'Mondial', 'Personalizado'
+    ],
+    'Camión': [
+      'Ford', 'Chevrolet', 'GMC', 'Ram', 'Toyota', 'Nissan', 'Isuzu', 'Mitsubishi', 'Hino', 'Fuso',
+      'Freightliner', 'International', 'Kenworth', 'Peterbilt', 'Mack', 'Volvo', 'Scania', 'MAN',
+      'Iveco', 'DAF', 'Renault Trucks', 'Mercedes-Benz', 'Volkswagen', 'Tata', 'Ashok Leyland', 'Personalizado'
+    ],
+    'Van': [
+      'Fox', 'Chevrolet', 'Nissan', 'Toyota', 'Mercedes-Benz', 'Volkswagen', 'Renault', 'Peugeot',
+      'Citroën', 'Fiat', 'Iveco', 'Ford Transit', 'Ram', 'GMC', 'Sprinter', 'Crafter', 'Daily',
+      'Master', 'Boxer', 'Jumper', 'Ducato', 'ProMaster', 'NV200', 'NV Cargo', 'Hiace', 'Proace',
+      'Transit', 'E-Series', 'Express', 'Savana', 'Personalizado'
+    ],
   };
 
   static const Map<String, Map<String, List<String>>> vehicleModels = {
     'Toyota': {
-      'Carro': ['Corolla', 'Camry', 'Prius', 'Avalon'],
+      'Carro': ['Corolla', 'Camry', 'Prius', 'Avalon', 'Yaris', 'GR86', 'Supra', 'Mirai'],
       'Sedán': ['Corolla', 'Camry', 'Prius', 'Avalon'],
-      'SUV': ['RAV4', 'Highlander', '4Runner', 'Sequoia'],
-      'Hatchback': ['Yaris', 'Prius C', 'Corolla Hatchback'],
-      'Van': ['Sienna', 'Hiace'],
+      'SUV': ['RAV4', 'Highlander', '4Runner', 'Sequoia', 'Land Cruiser', 'C-HR', 'Venza', 'bZ4X'],
+      'Hatchback': ['Yaris', 'Prius C', 'Corolla Hatchback', 'GR Yaris'],
+      'Van': ['Sienna', 'Hiace', 'Proace'],
       'Camión': ['Tacoma', 'Tundra'],
     },
     'Honda': {
-      'Carro': ['Civic', 'Accord', 'Insight'],
+      'Carro': ['Civic', 'Accord', 'Insight', 'Fit', 'HR-V'],
       'Sedán': ['Civic', 'Accord', 'Insight'],
-      'SUV': ['CR-V', 'Pilot', 'Passport', 'HR-V'],
-      'Hatchback': ['Civic Hatchback', 'Fit'],
-      'Motocicleta': ['CBR', 'CB', 'CRF', 'GL', 'VFR', 'NC'],
+      'SUV': ['CR-V', 'Pilot', 'Passport', 'HR-V', 'Ridgeline', 'Element'],
+      'Hatchback': ['Civic Hatchback', 'Fit', 'CR-Z'],
+      'Motocicleta': ['CBR', 'CB', 'CRF', 'GL', 'VFR', 'NC', 'Rebel', 'Shadow', 'Gold Wing', 'Africa Twin'],
       'Van': ['Odyssey'],
     },
     'Nissan': {
-      'Carro': ['Sentra', 'Altima', 'Maxima'],
+      'Carro': ['Sentra', 'Altima', 'Maxima', 'Versa', 'GT-R'],
       'Sedán': ['Sentra', 'Altima', 'Maxima'],
-      'SUV': ['Rogue', 'Murano', 'Pathfinder', 'Armada'],
-      'Hatchback': ['Versa', 'Kicks'],
-      'Camión': ['Frontier', 'Titan'],
-      'Van': ['NV200', 'NV Cargo'],
+      'SUV': ['Rogue', 'Murano', 'Pathfinder', 'Armada', 'Kicks', 'Juke', 'Qashqai'],
+      'Hatchback': ['Versa', 'Kicks', 'Leaf', 'Juke'],
+      'Camión': ['Frontier', 'Titan', 'Navara'],
+      'Van': ['NV200', 'NV Cargo', 'Quest'],
     },
     'Hyundai': {
-      'Carro': ['Elantra', 'Sonata', 'Ioniq'],
+      'Carro': ['Elantra', 'Sonata', 'Ioniq', 'Accent', 'Veloster'],
       'Sedán': ['Elantra', 'Sonata', 'Ioniq'],
-      'SUV': ['Tucson', 'Santa Fe', 'Palisade', 'Kona'],
-      'Hatchback': ['Veloster', 'Accent'],
-      'Van': ['Transit'],
+      'SUV': ['Tucson', 'Santa Fe', 'Palisade', 'Kona', 'Venue', 'Nexo'],
+      'Hatchback': ['Veloster', 'Accent', 'i20', 'i30'],
+      'Van': ['Transit', 'H-1'],
     },
     'Kia': {
-      'Carro': ['Forte', 'Optima', 'Stinger'],
+      'Carro': ['Forte', 'Optima', 'Stinger', 'Rio', 'Cerato'],
       'Sedán': ['Forte', 'Optima', 'Stinger'],
-      'SUV': ['Sportage', 'Sorento', 'Telluride', 'Seltos'],
-      'Hatchback': ['Rio', 'Soul'],
-      'Van': ['Sedona'],
+      'SUV': ['Sportage', 'Sorento', 'Telluride', 'Seltos', 'Niro', 'EV6'],
+      'Hatchback': ['Rio', 'Soul', 'Picanto'],
+      'Van': ['Sedona', 'Carnival'],
     },
     'Chevrolet': {
-      'Carro': ['Cruze', 'Malibu', 'Impala'],
+      'Carro': ['Cruze', 'Malibu', 'Impala', 'Sonic', 'Spark', 'Camaro'],
       'Sedán': ['Cruze', 'Malibu', 'Impala'],
-      'SUV': ['Equinox', 'Traverse', 'Tahoe', 'Suburban'],
+      'SUV': ['Equinox', 'Traverse', 'Tahoe', 'Suburban', 'Blazer', 'Trax', 'Bolt EV'],
       'Hatchback': ['Sonic', 'Spark'],
       'Camión': ['Silverado', 'Colorado'],
       'Van': ['Express'],
     },
     'Ford': {
-      'Carro': ['Focus', 'Fusion', 'Mustang'],
+      'Carro': ['Focus', 'Fusion', 'Mustang', 'Fiesta'],
       'Sedán': ['Focus', 'Fusion', 'Mustang'],
-      'SUV': ['Escape', 'Explorer', 'Expedition', 'Bronco'],
+      'SUV': ['Escape', 'Explorer', 'Expedition', 'Bronco', 'Edge', 'EcoSport'],
       'Hatchback': ['Fiesta', 'Focus Hatchback'],
       'Camión': ['F-150', 'F-250', 'F-350', 'Ranger'],
       'Van': ['Transit', 'E-Series'],
     },
     'Volkswagen': {
-      'Carro': ['Jetta', 'Passat', 'Arteon'],
+      'Carro': ['Jetta', 'Passat', 'Arteon', 'CC'],
       'Sedán': ['Jetta', 'Passat', 'Arteon'],
-      'SUV': ['Tiguan', 'Atlas', 'ID.4'],
-      'Hatchback': ['Golf', 'Polo'],
-      'Van': ['Transporter'],
+      'SUV': ['Tiguan', 'Atlas', 'ID.4', 'Touareg'],
+      'Hatchback': ['Golf', 'Polo', 'Beetle', 'Up!'],
+      'Van': ['Transporter', 'Crafter'],
     },
     'Mazda': {
-      'Carro': ['Mazda3', 'Mazda6'],
+      'Carro': ['Mazda3', 'Mazda6', 'MX-5'],
       'Sedán': ['Mazda3', 'Mazda6'],
-      'SUV': ['CX-3', 'CX-5', 'CX-9'],
+      'SUV': ['CX-3', 'CX-5', 'CX-9', 'CX-30'],
       'Hatchback': ['Mazda3 Hatchback', 'MX-5'],
     },
     'BMW': {
-      'Sedán': ['Serie 3', 'Serie 5', 'Serie 7'],
-      'SUV': ['X1', 'X3', 'X5', 'X7'],
-      'Motocicleta': ['R', 'K', 'G', 'F'],
+      'Sedán': ['Serie 3', 'Serie 5', 'Serie 7', 'Serie 1', 'Serie 2'],
+      'SUV': ['X1', 'X3', 'X5', 'X7', 'X2', 'X4', 'X6'],
+      'Hatchback': ['Serie 1', 'Serie 2 Active Tourer'],
+      'Motocicleta': ['R', 'K', 'G', 'F', 'C', 'S'],
     },
     'Mercedes-Benz': {
-      'Sedán': ['Clase C', 'Clase E', 'Clase S'],
-      'SUV': ['GLA', 'GLC', 'GLE', 'GLS'],
-      'Van': ['Sprinter'],
+      'Sedán': ['Clase C', 'Clase E', 'Clase S', 'Clase A', 'Clase B'],
+      'SUV': ['GLA', 'GLC', 'GLE', 'GLS', 'GLB', 'G-Class'],
+      'Hatchback': ['Clase A', 'Clase B'],
+      'Van': ['Sprinter', 'Vito', 'Citan'],
     },
     'Audi': {
-      'Sedán': ['A3', 'A4', 'A6', 'A8'],
-      'SUV': ['Q3', 'Q5', 'Q7', 'Q8'],
+      'Sedán': ['A3', 'A4', 'A6', 'A8', 'A1', 'A5'],
+      'SUV': ['Q3', 'Q5', 'Q7', 'Q8', 'Q2', 'Q4', 'Q6'],
+      'Hatchback': ['A1', 'A3 Sportback'],
+    },
+    'Lexus': {
+      'Sedán': ['IS', 'ES', 'GS', 'LS'],
+      'SUV': ['UX', 'NX', 'RX', 'GX', 'LX'],
+    },
+    'Infiniti': {
+      'Sedán': ['Q50', 'Q60', 'M37', 'G37'],
+      'SUV': ['QX30', 'QX50', 'QX60', 'QX80'],
+    },
+    'Acura': {
+      'Sedán': ['ILX', 'TLX', 'RLX'],
+      'SUV': ['CDX', 'RDX', 'MDX', 'ZDX'],
+    },
+    'Genesis': {
+      'Sedán': ['G70', 'G80', 'G90'],
+      'SUV': ['GV70', 'GV80'],
+    },
+    'Subaru': {
+      'Sedán': ['Impreza', 'Legacy', 'WRX'],
+      'SUV': ['Crosstrek', 'Forester', 'Outback', 'Ascent'],
+      'Hatchback': ['Impreza', 'WRX'],
+    },
+    'Volvo': {
+      'Sedán': ['S60', 'S90'],
+      'SUV': ['XC40', 'XC60', 'XC90'],
+      'Hatchback': ['V40', 'V60', 'V90'],
+    },
+    'Jaguar': {
+      'Sedán': ['XE', 'XF', 'XJ'],
+      'SUV': ['E-Pace', 'F-Pace', 'I-Pace'],
+    },
+    'Land Rover': {
+      'SUV': ['Range Rover Evoque', 'Range Rover Velar', 'Range Rover Sport', 'Range Rover', 'Defender', 'Discovery'],
+    },
+    'Porsche': {
+      'Sedán': ['Panamera', 'Taycan'],
+      'SUV': ['Macan', 'Cayenne'],
     },
     'Yamaha': {
-      'Motocicleta': ['YZF', 'FZ', 'MT', 'XT', 'WR', 'TMAX'],
+      'Motocicleta': ['YZF', 'FZ', 'MT', 'XT', 'WR', 'TMAX', 'XSR', 'FJR', 'VMAX', 'Bolt', 'Star'],
     },
     'Suzuki': {
-      'Motocicleta': ['GSX', 'GSF', 'DL', 'RM', 'DR'],
+      'Motocicleta': ['GSX', 'GSF', 'DL', 'RM', 'DR', 'V-Strom', 'Bandit', 'Katana'],
     },
     'Kawasaki': {
-      'Motocicleta': ['Ninja', 'Z', 'KLR', 'Versys', 'Vulcan'],
+      'Motocicleta': ['Ninja', 'Z', 'KLR', 'Versys', 'Vulcan', 'Concours', 'W800', 'KLX'],
+    },
+    'KTM': {
+      'Motocicleta': ['Duke', 'RC', 'Adventure', 'Enduro', 'Super Duke', 'Super Adventure'],
+    },
+    'Ducati': {
+      'Motocicleta': ['Monster', 'Panigale', 'Multistrada', 'Scrambler', 'Diavel', 'Streetfighter'],
+    },
+    'Harley-Davidson': {
+      'Motocicleta': ['Sportster', 'Softail', 'Touring', 'CVO', 'Street', 'LiveWire'],
+    },
+    'Aprilia': {
+      'Motocicleta': ['RSV4', 'Tuono', 'Shiver', 'Dorsoduro', 'Caponord', 'Pegaso'],
+    },
+    'Triumph': {
+      'Motocicleta': ['Speed Triple', 'Daytona', 'Tiger', 'Bonneville', 'Rocket', 'Scrambler'],
+    },
+    'Royal Enfield': {
+      'Motocicleta': ['Classic', 'Bullet', 'Continental GT', 'Himalayan', 'Interceptor'],
+    },
+    'Bajaj': {
+      'Motocicleta': ['Pulsar', 'Discover', 'Dominar', 'Avenger', 'CT'],
+    },
+    'TVS': {
+      'Motocicleta': ['Apache', 'Jupiter', 'Scooty', 'Ntorq', 'Raider'],
+    },
+    'Hero': {
+      'Motocicleta': ['Splendor', 'Passion', 'Karizma', 'Xtreme', 'Glamour'],
     },
     'GMC': {
       'SUV': ['Terrain', 'Acadia', 'Yukon'],
@@ -209,34 +310,68 @@ class VehicleImageService {
       'Van': ['ProMaster'],
     },
     'Isuzu': {
-      'Camión': ['D-Max', 'NPR'],
+      'Camión': ['D-Max', 'NPR', 'FTR'],
       'Van': ['NPR'],
     },
     'Mitsubishi': {
-      'Carro': ['Mirage', 'Lancer'],
-      'Sedán': ['Mirage', 'Lancer'],
-      'SUV': ['Outlander', 'Eclipse Cross'],
-      'Camión': ['L200'],
+      'Carro': ['Mirage', 'Lancer', 'Galant'],
+      'Sedán': ['Mirage', 'Lancer', 'Galant'],
+      'SUV': ['Outlander', 'Eclipse Cross', 'ASX', 'Montero'],
+      'Camión': ['L200', 'Triton'],
     },
     'Peugeot': {
-      'Hatchback': ['208', '308'],
+      'Hatchback': ['208', '308', '2008'],
       'Van': ['Partner', 'Expert'],
     },
     'Renault': {
-      'Hatchback': ['Clio', 'Megane'],
-      'Van': ['Kangoo', 'Master'],
+      'Hatchback': ['Clio', 'Megane', 'Captur'],
+      'Van': ['Kangoo', 'Master', 'Trafic'],
     },
-    'KTM': {
-      'Motocicleta': ['Duke', 'RC', 'Adventure', 'Enduro'],
+    'Fiat': {
+      'Hatchback': ['500', 'Punto', 'Tipo'],
+      'Van': ['Ducato', 'Doblo'],
     },
-    'Ducati': {
-      'Motocicleta': ['Monster', 'Panigale', 'Multistrada', 'Scrambler'],
+    'Seat': {
+      'Hatchback': ['Ibiza', 'Leon', 'Arona'],
+      'SUV': ['Ateca', 'Tarraco'],
     },
-    'Harley-Davidson': {
-      'Motocicleta': ['Sportster', 'Softail', 'Touring', 'CVO'],
+    'Skoda': {
+      'Hatchback': ['Fabia', 'Octavia', 'Kamiq'],
+      'SUV': ['Kodiaq', 'Karoq'],
     },
-    'Aprilia': {
-      'Motocicleta': ['RSV4', 'Tuono', 'Shiver', 'Dorsoduro'],
+    'Opel': {
+      'Hatchback': ['Corsa', 'Astra', 'Crossland'],
+      'SUV': ['Grandland', 'Mokka'],
+    },
+    'Jeep': {
+      'SUV': ['Renegade', 'Compass', 'Cherokee', 'Grand Cherokee', 'Wrangler', 'Gladiator'],
+    },
+    'Cadillac': {
+      'Sedán': ['ATS', 'CTS', 'XTS', 'CT6'],
+      'SUV': ['XT4', 'XT5', 'XT6', 'Escalade'],
+    },
+    'Lincoln': {
+      'Sedán': ['MKZ', 'Continental'],
+      'SUV': ['Corsair', 'Nautilus', 'Aviator', 'Navigator'],
+    },
+    'Buick': {
+      'Sedán': ['Regal', 'LaCrosse'],
+      'SUV': ['Encore', 'Envision', 'Enclave'],
+    },
+    'Mini': {
+      'Hatchback': ['Cooper', 'Cooper S', 'John Cooper Works', 'Clubman'],
+      'SUV': ['Countryman'],
+    },
+    'Smart': {
+      'Hatchback': ['Fortwo', 'Forfour'],
+    },
+    'Abarth': {
+      'Hatchback': ['500', '124 Spider'],
+    },
+    'Alfa Romeo': {
+      'Sedán': ['Giulia', 'Stelvio'],
+      'SUV': ['Stelvio'],
+      'Hatchback': ['Giulietta'],
     },
   };
 
